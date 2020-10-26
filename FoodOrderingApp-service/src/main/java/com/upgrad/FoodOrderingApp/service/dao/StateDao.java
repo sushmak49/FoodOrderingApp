40e3aa3fe803cf5761ca5e316a053a8bd70ba31a
manaswinisreekar <you@example.com>
2020-10-26 16:34:41 +0530

    Modified errors in StateDao

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/StateDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/StateDao.java
@@ -2,24 +2,24 @@ package com.upgrad.FoodOrderingApp.service.dao;
 
 import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 @Repository
 
 public class StateDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public StateEntity getsStateByUUID(final String UUID){
-        try{
-            return entityManager.createNamedQuery("getStateByUuid",StateEntity.class)
-                    .setParameter("StateUuid",stateUuid),
+    public StateEntity getStateByUUID(final String stateUuid) {
+        try {
+            return entityManager
+                    .createNamedQuery("getStateByUuid", StateEntity.class)
+                    .setParameter("stateUuid", stateUuid)
                     .getSingleResult();
-
-        } catch (NoResultException nre){
+        } catch (NoResultException nre) {
             return null;
         }
     }
