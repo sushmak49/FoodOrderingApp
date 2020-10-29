319912dade5b9014a75560d0df6809b5456cf389
shreshtha-hanspal <iguddu.hanspal@gmail.com>
2020-10-29 18:32:14 +0530

    Resolved merged conflicts

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/AddressEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/AddressEntity.java
@@ -1,115 +1,5 @@
-<<<<<<< HEAD
 package com.upgrad.FoodOrderingApp.service.entity;
 
-import javax.persistence.*;
-import java.util.Objects;
-
-@Entity
-@Table(name = "address", schema = "public", catalog = "restaurantdb")
-public class AddressEntity {
-    private int id;
-    private String uuid;
-    private String flatBuilNumber;
-    private String locality;
-    private String city;
-    private String pincode;
-    private Integer active;
-
-    @Id
-    @Column(name = "id", nullable = false)
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "uuid", nullable = false, length = 200)
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    @Basic
-    @Column(name = "flat_buil_number", nullable = true, length = 255)
-    public String getFlatBuilNumber() {
-        return flatBuilNumber;
-    }
-
-    public void setFlatBuilNumber(String flatBuilNumber) {
-        this.flatBuilNumber = flatBuilNumber;
-    }
-
-    @Basic
-    @Column(name = "locality", nullable = true, length = 255)
-    public String getLocality() {
-        return locality;
-    }
-
-    public void setLocality(String locality) {
-        this.locality = locality;
-    }
-
-    @Basic
-    @Column(name = "city", nullable = true, length = 30)
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Basic
-    @Column(name = "pincode", nullable = true, length = 30)
-    public String getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(String pincode) {
-        this.pincode = pincode;
-    }
-
-    @Basic
-    @Column(name = "active", nullable = true)
-    public Integer getActive() {
-        return active;
-    }
-
-    public void setActive(Integer active) {
-        this.active = active;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AddressEntity that = (AddressEntity) o;
-        return id == that.id &&
-                Objects.equals(uuid, that.uuid) &&
-                Objects.equals(flatBuilNumber, that.flatBuilNumber) &&
-                Objects.equals(locality, that.locality) &&
-                Objects.equals(city, that.city) &&
-                Objects.equals(pincode, that.pincode) &&
-                Objects.equals(active, that.active);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, uuid, flatBuilNumber, locality, city, pincode, active);
-    }
-}
-||||||| b0d6864
-=======
-package com.upgrad.FoodOrderingApp.service.entity;
-
-
-import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -117,7 +7,17 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -261,4 +161,3 @@ public class AddressEntity implements Serializable {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
->>>>>>> 4fc222cad2fd54eec1232b90932fbc2ec36957da
