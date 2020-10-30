1c9c90da9f43cf97ba0e1f6661fc3092b8f87c07
sushma.k <sushma.k@metricstream.com>
2020-10-30 20:03:42 +0530

    Added ItemService class

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,11 +1,21 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.ItemDao;
+import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrderItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrdersEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
     @Autowired
     private ItemDao itemDao;
+
+    public List<OrderItemEntity> getItemsByOrder(OrdersEntity ordersEntity) {
+        List<OrderItemEntity> orderItemEntityList = null;
+        return orderItemEntityList;
+    }
 }
