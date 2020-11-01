fbfb23019afcce63797e67d9c4b7d56ada1fe332
sushma.k <sushma.k@metricstream.com>
2020-11-01 17:00:11 +0530

    Included invalidRating and categoryNotFoundExceotion

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
@@ -36,7 +36,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(UpdateCustomerException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> UpdateCustomerException(
             UpdateCustomerException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
@@ -44,47 +44,57 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(CouponNotFoundException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> couponNotFoundException(
             CouponNotFoundException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AddressNotFoundException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> addressNotFoundException(
             AddressNotFoundException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> itemNotFoundException(
             ItemNotFoundException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RestaurantNotFoundException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> restaurantNotFoundException(
             RestaurantNotFoundException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PaymentMethodNotFoundException.class)
-    public ResponseEntity<ErrorResponse> Update(
+    public ResponseEntity<ErrorResponse> paymentMethodNotFoundException(
             PaymentMethodNotFoundException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> categoryNotFoundException(CategoryNotFoundException exc, WebRequest
+            request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()), HttpStatus.NOT_FOUND
+        );
+    }
 
-
-
+    @ExceptionHandler(InvalidRatingException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRatingException(InvalidRatingException ire, WebRequest webRequest) {
+        return new ResponseEntity<>(
+                new ErrorResponse().code(ire.getCode()).message(ire.getErrorMessage()), HttpStatus.BAD_REQUEST);
+    }
 }
